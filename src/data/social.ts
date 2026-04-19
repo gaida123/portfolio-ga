6d21fd3f50532403a7f3384396022355be262ad3
Gaida <78995933+gaida123@users.noreply.github.com>
2026-04-19 14:30:12 -0700

    updated instagram link

--- src/data/social.ts
+++ src/data/social.ts
@@ -1,7 +1,6 @@
-/** Update `instagram` to your profile URL */
 export const SOCIAL = {
-  linkedin: "https://www.linkedin.com/in/gaida-amzar-3304a82b6/",
+  linkedin: "https://www.linkedin.com/in/gaida-amzar/",
   github: "https://github.com/gaida123",
-  instagram: "https://www.instagram.com/gaida123",
+  instagram: "https://www.instagram.com/gaida.amz",
   email: "mailto:mgamzar@gmail.com",
 } as const;
